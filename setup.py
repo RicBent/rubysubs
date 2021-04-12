c288edf7686ab6f4857ebc79c6ca3b96bc2aca85
Bent <bent@mail.de>
2021-04-12 22:40:51 +0200

    Version bump to 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f_desc:
 
 setuptools.setup(
     name='rubysubs',
-    version='0.1.2',
+    version='0.1.3',
     author='Bent',
     author_email='bent@mail.de',
     description='Tool/library to add ruby text to subtitles',
